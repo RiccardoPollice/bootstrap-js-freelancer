99d109c6d9246b3074466b963c6a06b92685aea3
RiccardoPollice <riccardo.pollice17@gmail.com>
2023-11-27 12:04:40 +0100

    Lavoro progetto

--- js/main.js
+++ js/main.js
@@ -1,23 +1,53 @@
+let discountCodes = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"]
+
 function service(event) {
     event.preventDefault();
-
     let typeOfWork = document.getElementById("typeOfWork").value;
 
     let time = document.getElementById("hours").value;
     time = parseInt(time);
-
     let discountCode = document.getElementById("discountCode").value;
-
-    let price;
-
-    if (typeOfWork == "Backend") {
-        price = time * 20.50;
-    }  else if (typeOfWork == "Frontend") {
-        price = time * 15.30;
-    }  else if (typeOfWork == "Projectanalysis") {
-        price = time * 33.60;
+    let discountValid = false;
+    for (let i = 0; i < discountCodes.length; i++) {
+        if (discountCode === discountCodes[i]) {
+            discountValid = true;
+            discountCodes.splice(i, 1);
+            break;
+        }
+    }
+     if (discountValid) {
+        if (typeOfWork == "Backend") {
+            let price = time * 20.50 *0.75;
+            price = price.toFixed(2);
+            console.log(price);
+        }
+        else if (typeOfWork == "Frontend") {
+            let price = time * 15.30 * 0.75;
+            price = price.toFixed(2);
+            console.log(price);
+        }
+        else if (typeOfWork == "Projectanlaysis") {
+            let price = time * 15.30 * 0.75;
+            price = price.toFixed(2);
+            console.log(price);
+        };
+    }
+    else {
+        alert ("Codice sconto non valido")
+        if (typeOfWork == "Backend") {
+            let price = time *20.50;
+            console.log(price);
+        }
+        else if (typeOfWork == "Frontend") {
+            let price = time * 15.30;
+            console.log(price);
+        }
+        else if (typeOfWork == "PProjectanlaysis") {
+            let price = time * 33.60;
+            console.log(price);
+        }
     }
 
-    console.log(price);
-    
+
+
 }
