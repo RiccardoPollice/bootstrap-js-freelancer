eb03454796e86b363a14d09a42ee9bf25caa595e
RiccardoPollice <riccardo.pollice17@gmail.com>
2023-11-27 10:29:42 +0100

    Inizio lavoro su js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function service(event) {
+    event.preventDefault();
+    let typeOfWork= document.getElementById("typeOfWork");
+    let optionWork= typeOfWork.options[typeOfWork.selectedInedex].value;
+
+    
+
+}
+
+
+
+
+}
+
