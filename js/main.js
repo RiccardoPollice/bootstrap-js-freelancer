6e3f5cf3a951cbf17272336ec8939700dc197851
RiccardoPollice <riccardo.pollice17@gmail.com>
2023-11-27 11:15:45 +0100

    Avanzamneto progetto

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
-function service(event) {
-    event.preventDefault();
+function calculateService(event) {
+    calculateService.preventDefault();
+
     let typeOfWork= document.getElementById("typeOfWork");
-    let optionWork= typeOfWork.options[typeOfWork.selectedInedex].value;
+    let optionWork= typeOfWork.options[typeOfWork.selectedIndex].value;
 
-    
+    let time= document.getElementById("hours").value;
+    time= parseInt(time);
 
+    let discountCode= document.getElementById("discountCode").value;
+
+    if (optionService == "Backend") {
+        let price = time * 20.50
+        console.log(price);
+
+    } 
+
+    else if (optionService == "Frontend") {
+        let price = time * 15.30
+        console.log(price);
+
+    } 
+
+    else if (optionService == "Projectanalysis") {
+        let price = time * 33.60
+        console.log(price);
+    }
 }
-
-
-
-
-}
-
